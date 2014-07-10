e6a4028c42018df31e7155ee1086657639df4748
Lucas Constantino <lucas@taller.net.br>
2014-07-10 20:23:14 -0300

    Reapply no-html panel.

--- applications/default/public/js/controllers.js
+++ applications/default/public/js/controllers.js
@@ -72,7 +72,11 @@ angular.module('choko')
       }
 
       if ($scope.panel.bare) {
-        $scope.template = $scope.panel.template || 'templates/panel-content.html';
+        if ($scope.panel.html === false) {
+          $scope.template = $scope.panel.template || 'templates/panel-content-no-html.html';
+        } else {
+          $scope.template = $scope.panel.template || 'templates/panel-content.html';
+        }
       }
       else {
         $scope.template = 'templates/panel.html';

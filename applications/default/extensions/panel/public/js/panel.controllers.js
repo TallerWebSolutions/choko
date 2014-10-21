8ed4df10afd5f41cec18745aedbd8276616ab71a
Joseph Ramos <joseph.ramos@taller.net.br>
2014-10-21 12:47:28 -0500

    Adding extension controllers

---
+++ applications/default/extensions/panel/public/js/panel.controllers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('choko')
+
+.controller('PanelController', ['$scope', '$controller',
+  function ($scope, $controller) {
+    if ($scope.panel.type && $scope.panel.type !== 'default') {
+      // Set view to the panel itself and call ViewController.
+      $scope.view = $scope.panel;
+
+      // Inherit controller.
+      $controller('ViewController', {
+        $scope: $scope
+      });
+    }
+
+    if ($scope.panel.bare) {
+      if ($scope.panel.html === false) {
+        $scope.template = $scope.panel.template || 'templates/panel-content-no-html.html';
+      } else {
+        $scope.template = $scope.panel.template || 'templates/panel-content.html';
+      }
+    } else {
+      $scope.template = '/templates/panel.html';
+    }
+  }]);

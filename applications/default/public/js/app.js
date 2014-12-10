c06048d5ca1a8fe46865b9c2f837e562bbc66716
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-10 16:31:48 -0500

    Added little syntax modification

--- applications/default/public/js/app.js
+++ applications/default/public/js/app.js
@@ -5,7 +5,7 @@
  */
 
 // Define core choko dependencies.
-var dependencies = [  
+var dependencies = [
   'ngRoute',
   'ngResource',
   'ngSanitize',
@@ -23,53 +23,56 @@ angular.module('choko', dependencies)
   .value('version', '0.0.4')
 
   // Location/routing configuration.
-  .config(['$locationProvider', function($locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
-  }])
-  .config(['RestangularProvider', function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('/rest');
+  .config(['$locationProvider',
+    function($locationProvider) {
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+    }
+  ])
+  .config(['RestangularProvider',
+    function(RestangularProvider) {
+      RestangularProvider.setBaseUrl('/rest');
 
-    // @todo: We need improve the caching functionality.
-    // The items list not change after add an ítem, because
-    // he get the items list from the caché.
-    RestangularProvider.setDefaultHttpFields({cache: false});
+      // @todo: We need improve the caching functionality.
+      // The items list not change after add an ítem, because
+      // he get the items list from the caché.
+      RestangularProvider.setDefaultHttpFields({
+        cache: false
+      });
 
-    // Add a response intereceptor
-    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-      var extractedData;
+      // Add a response intereceptor
+      RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+        var extractedData;
 
-      if (operation === 'getList') {
-        var temp = [];
+        if (operation === 'getList') {
+          var temp = [];
 
-        Object.keys(data.data).forEach(function(name){
-          temp.push(data.data[name]);
-        });
-        extractedData = temp;
-      }
-      else if (operation === 'put') {
-        extractedData = data.data;
-        extractedData.updated = true;
-      }
-      else {
-        extractedData = data.data;
-      }
-      return extractedData;
-    });
+          Object.keys(data.data).forEach(function(name) {
+            temp.push(data.data[name]);
+          });
+          extractedData = temp;
+        } else if (operation === 'put') {
+          extractedData = data.data;
+          extractedData.updated = true;
+        } else {
+          extractedData = data.data;
+        }
+        return extractedData;
+      });
 
-    RestangularProvider.setResponseExtractor(function(response) {
-      var newResponse = response;
+      RestangularProvider.setResponseExtractor(function(response) {
+        var newResponse = response;
 
-      if (angular.isArray(response)) {
-        angular.forEach(newResponse, function(value, key) {
-          newResponse[key].originalElement = angular.copy(value);
-        });
-      }
-      else {
-        newResponse.originalElement = angular.copy(response);
-      }
-      return newResponse;
-    });
-  }]);
+        if (angular.isArray(response)) {
+          angular.forEach(newResponse, function(value, key) {
+            newResponse[key].originalElement = angular.copy(value);
+          });
+        } else {
+          newResponse.originalElement = angular.copy(response);
+        }
+        return newResponse;
+      });
+    }
+  ]);
